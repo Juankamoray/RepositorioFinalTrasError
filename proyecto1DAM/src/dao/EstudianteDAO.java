aa04ccc5db9e7a0c4f460fd984e547499a4b7ccc
Juankamoray <imjuanky@gmail.com>
2025-05-12 13:21:38 +0200

    segundo commit
    segundo commit sprint 2 tras solucionar el problema con commitear

--- proyecto1DAM/src/dao/EstudianteDAO.java
+++ proyecto1DAM/src/dao/EstudianteDAO.java
@@ -1,5 +1,56 @@
 package dao;
 
+import modelo.Estudiante;
+import util.ConexionBD;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EstudianteDAO {
 
+    public boolean añadirEstudiante(Estudiante estudiante) {
+        String sql = "INSERT INTO estudiantes (nombre, email, pais, fecha_nacimiento) VALUES (?, ?, ?, ?)";
+        try (Connection connect = ConexionBD.obtenerConexion();
+             PreparedStatement ps = connect.prepareStatement(sql)) {
+
+            ps.setString(1, estudiante.getNombre());
+            ps.setString(2, estudiante.getEmail());
+            ps.setString(3, estudiante.getPais());
+            ps.setDate(4, Date.valueOf(estudiante.getFechaNacimiento()));
+
+            ps.executeUpdate();
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error al insertar estudiante: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public List<Estudiante> obtenerTodos() {
+        List<Estudiante> lista = new ArrayList<>();
+        String sql = "SELECT * FROM estudiantes";
+
+        try (Connection con = ConexionBD.obtenerConexion();
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Estudiante est = new Estudiante();
+                est.setId(rs.getInt("id"));
+                est.setNombre(rs.getString("nombre_completo"));
+                est.setEmail(rs.getString("correo"));
+                est.setPais(rs.getString("pais"));
+                est.setFechaNacimiento(rs.getDate("fecha_nacimiento").toLocalDate());
+
+                lista.add(est);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error al obtener estudiantes: " + e.getMessage());
+        }
+
+        return lista;
+    }
 }
--- proyecto1DAM/src/main/Main.java
+++ proyecto1DAM/src/main/Main.java
@@ -1,10 +1,28 @@
 package main;
 
-import java.sql.Connection; 
-import util.ConexionBD;
+import dao.EstudianteDAO;
+import modelo.Estudiante;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-    
+
+        // Me creo como estudiante para comprobar si funciona la conexion a la BD
+        Estudiante est = new Estudiante();
+        est.setNombre("Juan Carlos Moreno Moray");
+        est.setEmail("juan.carlos.moreno.moray@salesianas.com");
+        est.setPais("España");
+        est.setFechaNacimiento(LocalDate.of(2006, 1, 21));
+
+        // Con DAO vamos a insertarme en la tabla
+        EstudianteDAO estudianteDAO = new EstudianteDAO();
+        boolean insertado = estudianteDAO.añadirEstudiante(est);
+
+        if (insertado) {
+            System.out.println("Se ha añadadido estudiante a la BD");
+        } else {
+            System.out.println("No se ha añadido el estudiante a la BD");
+        }
     }
 }
